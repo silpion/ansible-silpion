e44e0890911a6f458aefaa0bf449ff768816c6e5
Mark Kusch <mark.kusch@gmail.com>
2014-08-26 18:30:44 +0200

    [tdd] Split up Rakefile for Docker and Vagrant
    Provide Docker/Vagrant specific rake tasks in the rake/ directory
    and add this to the Ruby $LOAD_PATH in Rakefile.
    
    Allow to decide for Vagrant when exporting an environment variable
    RAKE_ANSIBLE_USE_VAGRANT, default to using Docker.

---
+++ rake/docker.rb
@@ -0,0 +1,29 @@
+require 'rake'
+require 'rspec/core/rake_task'
+
+require './spec/lib/docker'
+d = Docker.new
+
+
+desc "Bring up Docker container"
+task :up do
+  d.docker_pull
+  d.docker_run
+  d.ssh_available?
+end
+
+
+desc "Provision Docker container"
+task :provision => [:up] do
+  d.ansible_hosts_add
+  sh %{ansible-playbook --inventory-file #{d.ansible_hosts_file} --limit #{d.env_name} tests/playbook.yml}
+  d.ansible_hosts_del
+end
+
+
+desc "Cleanup Docker container environment"
+task :clean do
+  d.docker_stop
+  d.docker_rm
+  d.ansible_hosts_del
+end
